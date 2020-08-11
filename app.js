193c51a0da75c921e6356ec5931db052fb7d32cc
adrian154 <94568a@gmail.com>
2020-08-10 23:02:44 -0700

    fux bug

--- app.js
+++ app.js
@@ -26,8 +26,7 @@ let ws = new WebSocket("ws://localhost:1738");
 
 ws.on("message", data => {
     let obj = JSON.parse(data);
-    console.log(bot);
-    for(let guild of bot.guilds) {
+    for(let guild of bot.guilds.cache) {
         let channel = guild.channels.find(channel => channel.name === "mc");
         channel.send(data);
     }
